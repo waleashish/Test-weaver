b13f11c7418ff6e7bafb07f07d62078a7ff59c64
waleashish <ashishwale747@gmail.com>
2024-01-26 23:01:39 -0700

    Add JUnit file.

--- src/test/java/com/bje/testweaver/TestWeaverApplicationTests.java
+++ src/test/java/com/bje/testweaver/TestWeaverApplicationTests.java
@@ -1,13 +1,35 @@
 package com.bje.testweaver;
 
+import com.bje.testweaver.service.TestWeaverService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * This class contains unit test cases for the application flows.
+ *
+ * @author ashishwale
+ */
 @SpringBootTest
 class TestWeaverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static TestWeaverService testWeaverService;
+
+    @BeforeAll
+    static void setup() {
+        testWeaverService = new TestWeaverService();
+    }
+
+    @Test
+    void testGetTestCases() {
+        String code = "int add(int x, int y) {" +
+                "return x + y;" +
+                "}";
+        String framework = "JUnit5";
+
+        String output = testWeaverService.generateTestCases(code, framework);
+        Assertions.assertNotNull(output);
+    }
 
 }
